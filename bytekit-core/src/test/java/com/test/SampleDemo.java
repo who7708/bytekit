4dcaf514c7c748b15967f1817f5709199cc7d556
Chris <who7708@163.com>
2021-01-15 23:18:13 +0800

    增加hexdump

--- bytekit-core/src/test/java/com/test/SampleDemo.java
+++ bytekit-core/src/test/java/com/test/SampleDemo.java
@@ -1,6 +1,7 @@
 package com.test;
 
 import com.alibaba.bytekit.utils.AgentUtils;
+import com.alibaba.bytekit.utils.HexDump;
 
 /**
  * @author Chris
@@ -12,18 +13,21 @@ class SampleDemo {
     public static void main(String[] args) throws Exception {
 
         // 启动Sample
-        System.out.println("before retransform ...");
-        try {
-            Sample sample = new Sample();
-            sample.hello("1", false);
-            sample.hello("2", true);
-        } catch (Exception e) {
-            e.printStackTrace(System.out);
-        }
-        System.out.println();
+        // System.out.println("before retransform ...");
+        // try {
+        //     Sample sample = new Sample();
+        //     sample.hello("1", false);
+        //     sample.hello("2", true);
+        // } catch (Exception e) {
+        //     e.printStackTrace(System.out);
+        // }
+        // System.out.println();
 
         // 对Sample类的hello方法进行拦截处理，返回增强后的字节码
         byte[] bytes = EnhanceUtil.enhanceClass(Sample.class, new String[]{"hello"}, SampleInterceptor.class);
+
+        // HexDump.dump(bytes, 0, new FileOutputStream(new File("dum.txt")), 0);
+        HexDump.dump(bytes);
 
         // 查看反编译结果
         //System.out.println(Decompiler.decompile(bytes));
